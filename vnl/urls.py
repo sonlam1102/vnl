0d230719dc1c62a0f7c1e791bb3a9ce4a0ad28c5
Thanh Son <son.luu@youthdev.net>
2019-06-22 09:34:04 +0700

    Allow access static files

--- vnl/urls.py
+++ vnl/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+
+from vnl import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^location/', include('location.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
